b2441c7921b4f0c8f645cff982b736460efc3245
Garion Opiola <garion-opiola@student.kirkwood.edu>
2022-10-06 18:47:37 -0500

    Testing merge, and started map 1 and 2 test

--- tests/codingbat/Map1Test.java
+++ tests/codingbat/Map1Test.java
@@ -1,5 +1,11 @@
 package codingbat;
 
+import org.junit.jupiter.api.Test;
+
 class Map1Test {
 
+    @Test
+    void Garion_topping1(){
+
+    }
 }
--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -1,5 +1,10 @@
 package codingbat;
 
-class Map2Test {
+import org.junit.jupiter.api.Test;
 
+class Map2Test {
+    @Test
+    void Garion_word0(){
+
+    }
 }
