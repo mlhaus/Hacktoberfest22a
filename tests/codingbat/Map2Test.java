d66236fb89b78143508497fce6e1c7485d75e708
Brendan Klostermann <brendan.klostermann@gmail.com>
2022-10-06 19:19:40 -0500

    Completed tests for wordLen on Map2

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -2,7 +2,7 @@ package codingbat;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,7 +16,20 @@ class Map2Test {
         assertEquals(Arrays.toString(expected), Arrays.toString(actual));
     }
 
+    @Test
     void brendan_wordLen(){
+        String[] strings = {"words", "are", "counted", "by", "chars"};
+
+        Map<String,Integer>expected = new HashMap<>();
+        expected.put("words",5);
+        expected.put("are",3);
+        expected.put("counted",7);
+        expected.put("by",2);
+        expected.put("chars",5);
+
+        Map<String, Integer> actual = Map2.wordLen(strings);
+
+        assertEquals(expected,actual);
 
     }
 }
