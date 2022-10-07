52458480c5fc5d5e99b6facadf779265f9fc50da
SJPTL <parker.svoboda@gmail.com>
2022-10-06 21:13:46 -0500

    wordMultiple test added

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -3,6 +3,8 @@ package codingbat;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,5 +16,20 @@ class Map2Test {
         String[] actual = Map2.allSwap(strings);
         String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
         assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+    }
+
+    @Test
+    void parker_wordMultiple() {
+        String[] strings = {"apple","pear","pineapple","strawberry","apple","strawberry","durian"};
+        Map<String, Boolean> actual = Map2.wordMultiple(strings);
+        Map<String, Boolean> expected = new HashMap<>();
+        expected.put("apple",true);
+        expected.put("pear",false);
+        expected.put("pineapple",false);
+        expected.put("strawberry",true);
+        expected.put("apple",true);
+        expected.put("strawberry",true);
+        expected.put("durian",false);
+        assertEquals(expected,actual);
     }
 }
