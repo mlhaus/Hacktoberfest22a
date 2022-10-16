7dbfc647a2ab866501ddab38194711737db22dac
Parker Svoboda <parker.svoboda@gmail.com>
2022-10-16 14:40:16 -0500

    Prime.java added

---
+++ src/codewars/Prime.java
@@ -0,0 +1,41 @@
+package codewars;
+
+//Brought to you by Parker
+
+public class Prime {
+    public static boolean isPrime(int num) {
+        if(num < 0) {
+            if (num >= -1) {
+                return false;
+            }
+            if (num >= -3) {
+                return true;
+            }
+            if (num % -2 == 0 || num % -3 == 0) {
+                return false;
+            }
+            for (int i = -5; i * i >= num; i = i - 6) {
+                if (num % i == 0 || num % (i - 2) == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            if (num <= 1) {
+                return false;
+            }
+            if (num <= 3) {
+                return true;
+            }
+            if (num % 2 == 0 || num % 3 == 0) {
+                return false;
+            }
+            for (int i = 5; i * i <= num; i = i + 6) {
+                if (num % i == 0 || num % (i + 2) == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
