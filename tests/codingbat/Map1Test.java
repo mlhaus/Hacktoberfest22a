14379209c07f7d0350025f55683841d4211e1615
Elijah Morgan <Mineturtle505@gmail.com>
2022-10-20 00:30:06 -0500

    Finished Map1 and Map 2 Tests

--- tests/codingbat/Map1Test.java
+++ tests/codingbat/Map1Test.java
@@ -5,26 +5,20 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Map1Test {
 
     @Test
-    void marc_mapAB2() {
+    void eli_topping2() {
         Map<String, String> map1 = new HashMap<>();
-        map1.put("a", "cat");
-        map1.put("b", "cat");
-        Map<String, String> map2 = Map1.mapAB2(map1);
-        assertFalse(map2.containsKey("a"));
-        assertFalse(map2.containsKey("b"));
-
-        Map<String, String> map3 = new HashMap<>();
-        map3.put("a", "cat");
-        map3.put("b", "dog");
-        Map<String, String> map4 = Map1.mapAB2(map3);
-        assertTrue(map4.containsKey("a"));
-        assertTrue(map4.containsKey("b"));
+        map1.put("ice cream", "cherry");
+        map1.put("spinach", "dressing");
+        Map<String, String> map2 = new HashMap<>();
+        map2 = Map1.topping2(map1);
+        assertEquals("cherry", map2.get("yogurt"));
+        assertEquals("nuts", map2.get("spinach"));
+        assertTrue(map2.get("ice cream").equals(map2.get("yogurt")));
+        assertFalse(map2.get("spinach").equals("dressing"));
     }
-
 }
--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -3,16 +3,18 @@ package codingbat;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Map2Test {
 
     @Test
-    void marc_allSwap() {
-        String[] strings = {"cat", "dog", "bunny", "caterpillar", "kangaroo", "kitten", "butterfly", "crocodile", "centipede"};
-        String[] actual = Map2.allSwap(strings);
-        String[] expected = {"caterpillar", "dog", "butterfly", "cat", "kitten", "kangaroo", "bunny", "centipede", "crocodile"};
-        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+    void eli_wordAppend() {
+        String[] string1 = new String[] {"a", "b", "a", "", "b"};
+        String[] string2 = new String[] {"", "b", "a", "c", "c"};
+        assertEquals("ab", Map2.wordAppend(string1));
+        assertFalse(Map2.wordAppend(string1).equals(Map2.wordAppend(string2)));
     }
 }
