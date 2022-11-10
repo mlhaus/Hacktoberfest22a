79983ec1052cecf47330b1f860f21d43ed8208d6
Joey Philipp <k0788918@student.kirkwood.edu>
2022-11-09 22:24:31 -0800

    Wrote the pairs test for map2

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -93,7 +93,20 @@ class Map2Test {
     }
 
     @Test
-    void joey_firstSwap(){
+    void joey_pairs(){
+        String[] strings = {"Iowa", "Colorado", "Kansas", "Kentucky", "Vermont", "Virginia", "Georgia"};
+        Map<String, String> expected = new HashMap<>();
+        expected.put("I", "a");
+        expected.put("C", "o");
+        expected.put("K", "s");
+        expected.put("K", "y");
+        expected.put("V", "t");
+        expected.put("V", "a");
+        expected.put("G", "a");
+
+        Map<String, String> actual = Map2.pairs(strings);
+
+        assertEquals(expected, actual);
 
     }
 }
